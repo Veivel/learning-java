100325824696c199af5d07ebf22deaeef2ca7291
Veivel <givarrelveivelp@gmail.com>
2022-01-09 14:55:33 +0700

    s

--- src/com/tutorial/App.java
+++ src/com/tutorial/App.java
@@ -1,16 +1,19 @@
 package com.tutorial;
 
- endsWithSameWord(String a, String b) {
-    int i = b.indexOf(" ");
-    String lastWord = b.substring(i + 1);
-    return a.endsWith(lastWord);
-}
-
 public class App {
+    
+    // method that returns true/false if a & b end with the same word.
+    public static Boolean endsWithSameWord(String a, String b) {
+        int i = b.indexOf(" ");
+        String lastWord = b.substring(i + 1);
+        return a.endsWith(lastWord);
+    }
     public static void main(String[] args) {
         String myStr = "hello world!";
         String ourStr = "goodbye world!";
+        String newStr = "world! lol.";
+        String xStr = "world!";
 
-        System.out.println(endsWithSameWord(myStr, ourStr));
+        System.out.println(endsWithSameWord(xStr, myStr));
     }
 }
