6f2bf754c1cb024e41fc84d1a296ca04065be57e
Veivel <givarrelveivelp@gmail.com>
2022-01-09 14:26:08 +0700

    actually learning logic

--- src/com/tutorial/App.java
+++ src/com/tutorial/App.java
@@ -1,6 +1,16 @@
 package com.tutorial;
+
+ endsWithSameWord(String a, String b) {
+    int i = b.indexOf(" ");
+    String lastWord = b.substring(i + 1);
+    return a.endsWith(lastWord);
+}
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        }
+        String myStr = "hello world!";
+        String ourStr = "goodbye world!";
+
+        System.out.println(endsWithSameWord(myStr, ourStr));
+    }
 }
