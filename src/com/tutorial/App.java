e6d33d33f66fb02557a8015e855729e47ea3e766
Veivel <givarrelveivelp@gmail.com>
2022-01-16 11:17:49 +0700

    simple terminal calcu, draft v1

--- src/com/tutorial/App.java
+++ src/com/tutorial/App.java
@@ -1,19 +1,45 @@
 package com.tutorial;
+import java.util.*;
 
 public class App {
     
-    // method that returns true/false if a & b end with the same word.
-    public static Boolean endsWithSameWord(String a, String b) {
-        int i = b.indexOf(" ");
-        String lastWord = b.substring(i + 1);
-        return a.endsWith(lastWord);
+    public static Double calculate(Double a, Double b, String op){
+        switch(op) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                return 9999999999999.999999999;
+        }
+
     }
     public static void main(String[] args) {
-        String myStr = "hello world!";
-        String ourStr = "goodbye world!";
-        String newStr = "world! lol.";
-        String xStr = "world!";
+        List<String> validOps = new ArrayList<String>();
+        validOps.add("+");
+        validOps.add("-");
+        validOps.add("*");
+        validOps.add("/");
 
-        System.out.println(endsWithSameWord(xStr, myStr));
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("First value:");
+        Double numA = userInput.nextDouble();
+        System.out.println("Second value:");
+        Double numB = userInput.nextDouble();
+        System.out.println("Operator (+, -, *, /):");
+        String operator = userInput.next();
+
+        Double res = calculate(numA, numB, operator);
+        String x = validOps.contains(operator) ? res.toString() : "invalid operator";
+        System.out.println(x);
+
+        userInput.close();
     }
+
 }
