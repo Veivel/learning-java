6950473e5be0843e89c1b909367a721576e9355b
Veivel <givarrelveivelp@gmail.com>
2022-01-29 17:35:10 +0700

    ok

--- src/com/tutorial/App.java
+++ src/com/tutorial/App.java
@@ -1,52 +1,6 @@
 package com.tutorial;
 import java.util.*;
 
-class Coords {
-    Long x;
-    Long y;
-
-    Coords(Long x, Long y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    void move(Long a, Long b) {
-        this.x += a;
-        this.y += b;
-    }
-
-    public String display() {
-        return x.toString() + ", " + y.toString();
-    }
-
-    Boolean compare(Coords otherPoint) {
-        if (otherPoint.x == this.x && otherPoint.y == this.y) {
-            return true;
-        } 
-        else {
-            return false;
-        }
-    }
-
-    // only for start
-    Boolean checkX(Long targetX) {
-        if (this.x == targetX) return true;
-        if (this.y == 0) return false;
-
-        if ((targetX - this.x) % this.y == 0) return true;
-        else return false;
-    }
-
-    Boolean checkY(Long targetY) {
-        if (this.y == targetY) return true;
-        if (this.x == 0) return false;
-
-        if ((targetY - this.y) % this.x == 0) return true;
-        else return false;
-    }
-
-}
-
 public class App {
 
     public static Boolean isPathPossible(Coords start, Coords target) {
